ab7282c90cd8660cb194f285a8c30d0038d2daf8
quericy <quericy@live.com>
2015-04-01 14:02:14 +0800

    test_end

--- application/controllers/test.php
+++ application/controllers/test.php
@@ -7,15 +7,15 @@
  */
 class test extends CI_Controller{
     public function lala(){
- //       $this->load->database();
-        global $RUNTIME_DOMAIN;
-        echo ' global:'.$RUNTIME_DOMAIN;
-        if(isset($_SERVER['RUNTIME_DOMAIN'])&&!empty($_SERVER['RUNTIME_DOMAIN'])){
-            echo ' $server:'.$_SERVER['RUNTIME_DOMAIN'];
-        }
-
-        $this->smarty->assign('title','测试Smarty');
+//        $this->load->database();
+//        global $RUNTIME_DOMAIN;
+//        echo ' global:' . $RUNTIME_DOMAIN;
+//        if (isset($_SERVER['RUNTIME_DOMAIN']) && !empty($_SERVER['RUNTIME_DOMAIN'])) {
+//            echo ' $server:' . $_SERVER['RUNTIME_DOMAIN'];
+//        }
+//
+//        $this->smarty->assign('title', '测试Smarty');
 //        $this->smarty->assign('base_url',$this->config->item('base_url'));
-        $this->smarty->view('test.tpl');
+//        $this->smarty->view('test.tpl');
     }
 }
