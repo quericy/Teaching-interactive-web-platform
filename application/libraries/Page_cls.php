2621b9f9a758453470a3dbdcf0f07a8704f31d49
quericy <quericy@live.com>
2015-04-08 00:18:50 +0800

    分页当前页样式fix

--- application/libraries/Page_cls.php
+++ application/libraries/Page_cls.php
@@ -59,7 +59,7 @@ class Page_cls
         $config['prev_tag_open'] = '<li class="prev page">';
         $config['prev_tag_close'] = '</li>';
 
-        $config['cur_tag_open'] = '<li class="active"><a  href="">';
+        $config['cur_tag_open'] = '<li class="active"><a  href="#">';
         $config['cur_tag_close'] = '</a></li>';
 
         $config['num_tag_open'] = '<li class="page">';
