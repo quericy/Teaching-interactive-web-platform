7513e2a905c7eec1b3edbb673338c3e41b8bcde5
quericy <quericy@live.com>
2015-05-04 19:35:27 +0800

    保存系统设置后台逻辑

--- application/controllers/admin/sys_setting.php
+++ application/controllers/admin/sys_setting.php
@@ -18,6 +18,8 @@ class sys_setting extends CI_Controller
         $this->assign_arr['controller_name'] = $this->router->class;
         $this->assign_arr['web_title'] = '系统参数';
         $this->assign_arr['nav_show'] = 'system';
+
+        $this->load->helper('file');
     }
 
     public function index()
@@ -31,6 +33,22 @@ class sys_setting extends CI_Controller
      */
     public function save_setting()
     {
+        $web_name=$this->input->post('web_name',true);
+        $cookie_time=$this->input->post('cookie_time',true);
+        $reset_pwd=$this->input->post('reset_pwd',true);
+        $user_status=$this->input->post('user_status',true);
+        $power_reset_pwd=$this->input->post('power_reset_pwd',true);
+        $power_data_view=$this->input->post('power_data_view',true);
+        $power_log_view=$this->input->post('power_log_view',true);
+        if(!is_numeric($cookie_time)){
+            echo -2;
+        }
+        $power_reset_pwd=$power_reset_pwd=='1'?1:0;
+        $power_data_view=$power_data_view=='1'?1:0;
+        $power_log_view=$power_log_view=='1'?1:0;
+
+
+
         echo 1;
     }
 
